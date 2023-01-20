b38424c7658e7b65193abe79d30e4ed9cc58c2c7
Steven Tang <yc@steventang.net>
2023-01-21 10:18:11 +1100

    Debug emails

--- utils/dns.go
+++ utils/dns.go
@@ -3,6 +3,8 @@ package utils
 import (
 	"net"
 	"strings"
+
+	"github.com/emvi/logbuch"
 )
 
 // CheckEmailMX takes an e-mail address and verifies that an MX DNS record exists for its domain
@@ -12,5 +14,11 @@ func CheckEmailMX(email string) bool {
 		return false
 	}
 	records, err := net.LookupMX(parts[1])
+	if len(records) == 0 {
+		logbuch.Info("0 dns records")
+	}
+	if err != nil {
+		logbuch.Info("dns:" + err.Error())
+	}
 	return len(records) > 0 && err == nil
 }
